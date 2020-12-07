58b0f3ffc47f3f45795b29f26db2efdae5837800
Kris <kmazjanis@gmail.com>
2020-12-07 09:43:18 +0200

    Change to more readable synthax

--- index.php
+++ index.php
@@ -4,33 +4,32 @@ include_once "header.php";
 
 <title>Posts</title>
 <h3>Posts</h3>
+
 <?php
 require_once "includes/db.inc.php";
 require_once "includes/functions.inc.php";
  
-
 $sql = "SELECT * FROM post;";
 $result = mysqli_query($conn, $sql);
 $resultCheck = mysqli_num_rows($result);
-
-if ($resultCheck > 0) {
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<div class = article>";
-    echo "<form method='POST'>";
-    echo $row['title'] . "/";
-    echo $row['seo_title'] . "<br>";
-    echo $row['content'] . "<br>";
-    echo "<a href='edit.php?id=" . $row['id'] . "' class='btn btn-primary'>Edit</a>";
-    echo "<a href='delete.php?id=" . $row['id'] . "' class='btn btn-danger'>Delete</a>";
-    echo "</form>";
-    echo "</div>";
-}
-}
-
 ?>
+
+<?php if ($resultCheck > 0) { ?>
+   <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+        <div class = article>
+            <form method='POST'>
+            <?php echo $row['title'];?> /
+            <?php echo $row['seo_title'];?> <br>
+            <?php echo $row['content'];?> <br>
+            <a href='edit.php?id="<?php $row['id'];?>"' class='btn btn-primary'>Edit</a>
+            <a href='delete.php?id="<?php $row['id'];?>"' class='btn btn-danger'>Delete</a>
+            </form>
+        </div>
+    <?php }?>
+<?php }?>
+
 
 
 <?php
 include_once "footer.php";
 ?>
-
